272c3fd2fe3f2ee885bcace05b1e73f96e1f79c9
babankbro <babankbro@gmail.com>
2025-06-22 23:33:46 +0700

    fixed Menu sub

--- src/components/dashboard/layout/config.ts
+++ src/components/dashboard/layout/config.ts
@@ -3,22 +3,28 @@ import { paths } from "@/paths";
 
 export const navItems = [
 	{ key: "overview", title: "Overview", href: paths.dashboard.overview, icon: "chart-pie" },
-	{ key: "components", title: "Components",  icon: "boat" ,
+	{ 
+		key: "components", 
+		title: "Components", 
+		icon: "boat",
 		items: [
-		{ key: "customers", title: "Customers", href: paths.dashboard.customers, icon: "users" },
-		{ key: "barges", title: "Barge", href: paths.dashboard.barges, icon: "boat" },
-		{ key: "carriers", title: "Carrier", href: paths.dashboard.carriers, icon: "boat" },
-		{ key: "tugboats", title: "Tugboat", href: paths.dashboard.tugboats, icon: "boat" },
-		{ key: "stations", title: "Stations", href: paths.dashboard.stations, icon: "garage" }
-	]},
+			{ key: "customers", title: "Customers", href: paths.dashboard.customers, icon: "users" },
+			{ key: "barges", title: "Barge", href: paths.dashboard.barges, icon: "boat" },
+			{ key: "carriers", title: "Carrier", href: paths.dashboard.carriers, icon: "boat" },
+			{ key: "tugboats", title: "Tugboat", href: paths.dashboard.tugboats, icon: "boat" },
+			{ key: "stations", title: "Stations", href: paths.dashboard.stations, icon: "garage" },
+		]
+	},
 	{ key: "orders", title: "Orders", href: paths.order, icon: "order" },
-	{ key: "results", title: "Report Results",  icon: "boat" ,
+	{ 
+		key: "results", 
+		title: "Report Results", 
+		icon: "money",
 		items: [
-		{ key: "tugboat consumption", title: "Tugboat Consumption", href: paths.dashboard.costs, icon: "money" },
-		{ key: "tugboat schedule", title: "Tugboat Schedule", href: paths.dashboard.costs, icon: "boat" },
-		{ key: "barge usage", title: "Barge Usage", href: paths.dashboard.costs, icon: "boat" },
-		{ key: "grants", title: "Tugboat Grantt", href: paths.dashboard.costs, icon: "boat" },
-	]},
-
-	
+			{ key: "tugboat_consumption", title: "Tugboat Consumption", href: paths.dashboard.costs, icon: "money" },
+			{ key: "tugboat_schedule", title: "Tugboat Schedule", href: paths.dashboard.costs, icon: "boat" },
+			{ key: "barge_usage", title: "Barge Usage", href: paths.dashboard.costs, icon: "boat" },
+			{ key: "grants", title: "Tugboat Grant", href: paths.dashboard.costs, icon: "boat" },
+		]
+	}
 ] satisfies NavItemConfig[];
